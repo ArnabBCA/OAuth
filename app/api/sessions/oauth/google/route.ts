0e9435b1352f41975f061861e44646b3933a74f9
Arnab Ghosh <arnabsonu2@gmail.com>
2024-12-04 22:25:28 +0530

    added getGoogleUser

--- app/api/sessions/oauth/google/route.ts
+++ app/api/sessions/oauth/google/route.ts
@@ -46,6 +46,40 @@ export async function getGoogleOAuthTokens({
   }
 }
 
+interface GoogleUserResult {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  locale: string;
+}
+
+export async function getGoogleUser({
+  id_token,
+  access_token,
+}: {
+  id_token: string;
+  access_token: string;
+}): Promise<GoogleUserResult> {
+  try {
+    const res = await axios.get<GoogleUserResult>(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${id_token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error: any) {
+    console.error(error, "Error fetching Google user");
+    throw new Error(error.message);
+  }
+}
+
 export async function GET(request: Request): Promise<NextResponse> {
   try {
     const url = new URL(request.url);
@@ -57,13 +91,15 @@ export async function GET(request: Request): Promise<NextResponse> {
         { status: 400 }
       );
     }
-    
+
     const tokens = await getGoogleOAuthTokens({ code });
     const { id_token, access_token } = tokens;
 
-    console.log("Tokens received", { id_token, access_token });
+    //console.log("Tokens received", { id_token, access_token });
+    //const googleUser = jwt.decode(id_token);
+    const googleUser = await getGoogleUser({ id_token, access_token });
 
-    return NextResponse.json({ id_token, access_token });
+    return NextResponse.json(googleUser);
   } catch (error) {
     console.error("Error in Google OAuth Handler:", error);
     return NextResponse.json({ error: "OAuth failed" }, { status: 500 });
